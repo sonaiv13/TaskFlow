2cc5ebe28430a7dcc82eaf8f4afc7c86c09b3898
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-02-27 12:45:06 +0100

    userService created - create and get user by id methods.

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/users"; //url base backend
+
+export const createUser = async (user) => {
+    try {
+        const response = await axios.post(API_URL, user);
+        return response.data;
+    } catch (error) {
+        if(error.response) throw error.response.data; // errores del backend
+        throw error;
+    }
+};
+
+export const getUserById = async (id) => {
+    const response = await axios.get(`${API_URL}/${id}`);
+    return response.data;
+}
