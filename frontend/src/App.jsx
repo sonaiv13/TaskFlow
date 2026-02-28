5bed2ee0b5844008cbc857bf2d3e78e19c6ccddd
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-02-28 16:51:02 +0100

    App - Login set to use LoginForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,33 @@
-import UserForm from "./components/UserForm";
-import TaskForm from "./components/TaskForm.jsx";
+import {useEffect, useState} from "react";
+import {getCurrentUser, logout} from "./services/authService.js";
+import LoginForm from "./components/LoginForm.jsx";
 
 function App() {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const storedUser = getCurrentUser();
+        if(storedUser) {
+            setUser(storedUser);
+        }
+    }, []);
+
+    const handleLogout = () => {
+        logout();
+        setUser(null);
+    };
+
     return (
       <div style={{ padding: '20px' }}>
-          <h1>TaskFlow App</h1>
-          <h2>Crear Usuario</h2>
-          <UserForm />
-          <hr/>
-          <h2>Crear Tarea</h2>
-          <TaskForm />
+          {!user ? (
+              <LoginForm onLogin={setUser}/>
+          ) : (
+              <div>
+                  <h2>Bienvenida {user.name}</h2>
+                  <button onClick={handleLogout}>Cerrar sesión</button>
+              </div>
+          )}
       </div>
     );
 }
