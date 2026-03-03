6ae7f868d900f4b00c444a35df24794b1bb9f588
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-03-03 14:27:37 +0100

    Register logic - Password encoding and register user validation

--- backend/src/main/java/com/example/taskflow/service/UserService.java
+++ backend/src/main/java/com/example/taskflow/service/UserService.java
@@ -1,8 +1,12 @@
 package com.example.taskflow.service;
 
+import com.example.taskflow.dto.RegisterRequest;
+import com.example.taskflow.dto.UserResponse;
 import com.example.taskflow.exception.ResourceNotFoundException;
 import com.example.taskflow.model.User;
 import com.example.taskflow.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,18 +15,33 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     //Constructor con inyección de dependencias
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     //Registrar usuario
-    public User register(User user){
-        if(userRepository.findByEmail(user.getEmail()).isPresent()){
-            throw new RuntimeException("El email ya existe.");
+    public UserResponse register(RegisterRequest request) {
+        if(userRepository.findByEmail(request.getEmail()).isPresent()){
+            throw new RuntimeException("El usuario con este email ya existe.");
         }
-        return userRepository.save(user);
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+
+        // No guardar password en claro
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        User savedUser = userRepository.save(user);
+
+        return new UserResponse(
+                savedUser.getId(),
+                savedUser.getName(),
+                savedUser.getEmail()
+        );
     }
 
     //Iniciar Sesión
