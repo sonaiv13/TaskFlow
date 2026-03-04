d8ac5b54c8a5d9a04aaed08d9bd7417b44a37cac
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-03-04 16:10:05 +0100

    App.jsx modified to show Tasks list when logged in. And saves user when registers or login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import {useEffect, useState} from "react";
 import {getCurrentUser, logout} from "./services/authService.js";
 import LoginForm from "./components/LoginForm.jsx";
 import RegisterForm from "./components/RegisterForm.jsx";
+import TaskList from "./components/TaskList.jsx";
 
 function App() {
 
     const [user, setUser] = useState(null);
     const [showRegister, setShowRegister] = useState(false);
 
+    // Revisar si hay usaurio guardado en LocalStorage
     useEffect(() => {
         const storedUser = getCurrentUser();
         if(storedUser) {
@@ -15,29 +17,40 @@ function App() {
         }
     }, []);
 
+    //Cerrar sesión
     const handleLogout = () => {
         logout();
         setUser(null);
     };
 
+    // Si no hay usuario logueado
     if(!user){
         return showRegister ? (
-            <RegisterForm
-                onRegister={() => setShowRegister(false)}
-                onSwitch={() => setShowRegister(false)}
-            />
+            <div style={{ padding: '20px' }}>
+                <h1>TaskFlow - Registro</h1>
+                <RegisterForm
+                    onRegister={(newUser) => setUser(newUser)}
+                    onSwitch={() => setShowRegister(false)}
+                />
+            </div>
         ) : (
-            <LoginForm
-                onLogin={setUser}
-                onSwitch={() => setShowRegister(true)}
-            />
+            <div style={{ padding: '20px' }}>
+                <h1>TaskFlow - Login</h1>
+                <LoginForm
+                    onLogin={(loggedUser) => setUser(loggedUser)}
+                    onSwitch={() => setShowRegister(true)}
+                />
+            </div>
         );
     }
 
+    //Si hay usuario logueado
     return (
       <div style={{ padding: '20px' }}>
           <h2>Bienvenida {user.name}</h2>
           <button onClick={handleLogout}>Cerrar sesión</button>
+
+          <TaskList user={user}/>
       </div>
     );
 }
