d4b82bb6d7deac0e812c615607c98a00dd2375c1
BlackFrisian13 <sonaivanyan13@gmail.com>
2026-03-05 22:10:56 +0100

    taskService modified to send token in requests.

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -2,9 +2,16 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/tasks";
 
+const getAuthHeader = () => {
+  const token = localStorage.getItem("token");
+  return {
+      headers: { Authorization: `Bearer ${token}` }
+  };
+};
+
 export const createTask = async (userId, task) => {
     try {
-        const response = await axios.post(`${API_URL}/${userId}`, task);
+        const response = await axios.post(`${API_URL}/${userId}`, task, getAuthHeader());
         return response.data;
     } catch (error) {
         if(error.response) throw error.response.data;
@@ -14,7 +21,7 @@ export const createTask = async (userId, task) => {
 
 export const getTasks = async (userId) => {
     try {
-        const response = await axios.get(`${API_URL}/${userId}`);
+        const response = await axios.get(`${API_URL}/${userId}`, getAuthHeader());
         return response.data;
     } catch (error) {
         if(error.response) throw error.response.data;
@@ -24,7 +31,7 @@ export const getTasks = async (userId) => {
 
 export const updateTask = async (userId, taskId, task) => {
     try {
-        const response = await axios.put(`${API_URL}/${userId}/${taskId}`, task);
+        const response = await axios.put(`${API_URL}/${userId}/${taskId}`, task, getAuthHeader());
         return response.data;
     } catch (error) {
         if(error.response) throw error.response.data;
@@ -34,7 +41,7 @@ export const updateTask = async (userId, taskId, task) => {
 
 export const deleteTask = async (userId, taskId) => {
     try {
-        await axios.delete(`${API_URL}/${userId}/${taskId}`);
+        await axios.delete(`${API_URL}/${userId}/${taskId}`, getAuthHeader());
     }  catch (error) {
         if(error.response) throw error.response.data;
         throw error;
